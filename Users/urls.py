212aa02e7caa976f868396967e5dc18d0c16c4f7
unknown <ernir17@ru.is>
2019-05-07 10:36:45 +0000

    worked on authentication urls

--- Users/urls.py
+++ Users/urls.py
@@ -1,8 +1,10 @@
-
+from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name="users-index"),
+    path('register', views.register, name='register'),
+    path('login', LoginView.as_view(template_name='login.html'), name='login'),
     path('register', views.register, name='register')
 ]
