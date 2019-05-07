d66b56d32c69e432803154e8c9a966ffc1517d63
Bjarturl <bjartur17@ru.is>
2019-05-07 13:39:15 +0000

    fix user profile

--- Users/views.py
+++ Users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+from Users.forms.profile_form import ProfileForm
+from Users.models import Profiles
+
+
 def index(request) :
     return render(request, 'Users/index.html' )
 
