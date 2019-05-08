6aa9df585ddf4039172d62d95b0a352d2e8dc819
erlendur17 <erlendurj@gmail.com>
2019-05-08 08:19:09 +0000

    Added a form for uploading images TODO: make changes to database so it works!

--- Properties/urls.py
+++ Properties/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="properties-index"),
     path('create', views.createProperties, name="createProperties"),
-    path('<int:id>', views.getPropertyById, name="propertyDetails")
+    path('<int:id>', views.getPropertyById, name="propertyDetails"),
+    path('upload', views.propertiesImagesUpload, name="propertyImages")
 ]
--- Properties/views.py
+++ Properties/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from Properties.forms.propertiesForm import PropertiesCreateForm
+from Properties.forms.propertiesImagesForm import PropertiesImagesForm
 from Properties.models import Properties, Description
 
 
@@ -25,4 +26,17 @@ def createProperties(request):
 def getPropertyById(request, id):
     return render(request, 'Properties/propertyDetails.html', {
         'Properties': get_object_or_404(Properties, pk=id)
+    })
+
+
+def propertiesImagesUpload(request):
+    if request.method == 'POST':
+        form = PropertiesImagesForm(request.POST, request.FILES)
+        if form.is_valid():
+            properties = form.save()
+            return redirect('home')
+    else:
+        form = PropertiesImagesForm()
+    return render(request, 'Properties/uploadPropertyImages.html', {
+        'form': form
     })
