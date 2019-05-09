397baa3d211d6ac444521b618e7260cd9c4f9033
Bjarturl <bjartur17@ru.is>
2019-05-09 12:24:40 +0000

    added visual for filter

--- Properties/views.py
+++ Properties/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from Properties.forms.properties_form import PropertiesCreateForm
 from Properties.forms.properties_images_form import PropertiesImagesForm
-from Properties.models import Properties, Description, OpenHouses
+from Properties.models import Properties, Description, OpenHouses, Categories, Zip
 
 
 def index(request):
@@ -43,7 +43,10 @@ def upload_properties_images(request):
 
   
 def get_all_properties(request):
-    context = {'Properties': Properties.objects.all()}
+    context = {'Properties': Properties.objects.all(),
+               'Categories': Categories.objects.all(),
+               'Zip': Zip.objects.all()
+              }
     return render(request, 'Properties/index.html', context)
 
 
