4ae22f7b3266290154da1860cb98e180d1a7ac01
unknown <ernir17@ru.is>
2019-05-14 12:30:13 +0000

    worked on edit property picture 'add photo'

--- Properties/views.py
+++ Properties/views.py
@@ -42,15 +42,6 @@ def create_properties(request):
     })
 
 
-def create_properties_images(request):
-    if request.method == 'POST':
-        form = PropertiesImagesForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = PropertiesImagesForm()
-
-
 def update_property(request, id):
     if request.method == 'POST':
         form = PropertiesCreateForm(data=request.POST, instance=request.user, files=request.FILES)
@@ -100,12 +91,18 @@ def update_property_images(request, id):
     })
 
 
-def add_property_image(request):
+def add_property_image(request, id):
     if request.method == 'POST':
-        if True:
-            return
+        form = PropertiesImagesForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(update_property_images, id)
     else:
-        return
+        form = PropertiesImagesForm(initial={'property': id})
+    return render(request, 'Properties/upload_property_images.html', {
+        'form': form,
+        'prop_id': id
+    })
 
 
 def delete_property_image(request, id):
@@ -134,19 +131,6 @@ def get_property_by_id(request, id):
                    'Favourites': [f.property for f in Favourites.objects.filter(user=request.user.id)],
                    'PropertyVisits': PropertyVisits.objects.filter(property_id=id).aggregate(count=Sum('counter'))
                    })
-
-
-def upload_properties_images(request):
-    if request.method == 'POST':
-        form = PropertiesImagesForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('allProperties')
-    else:
-        form = PropertiesImagesForm()
-    return render(request, 'Properties/upload_property_images.html', {
-        'form': form
-    })
 
 
 def get_all_properties(request):
