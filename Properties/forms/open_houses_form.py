a79d067ffafec3528f001981aeffd8c6337a06b4
unknown <ernir17@ru.is>
2019-05-16 23:37:02 +0000

    commented add open house form

--- Properties/forms/open_houses_form.py
+++ Properties/forms/open_houses_form.py
@@ -17,10 +17,14 @@ class OpenHousesCreateForm(ModelForm):
         }
 
     def __init__(self, *args, **kwargs):
+        # Access to request inside this form
         self.request = kwargs.pop("request")
+        # All properties the current user is selling
         users_properties = PropertySellers.objects.filter(user_id=self.request.user.id)
         tmp_list = []
+        # Make tmp_list store all the property id's that the current user is selling in an array
         for i in users_properties:
             tmp_list.append(i.property_id)
         super(OpenHousesCreateForm, self).__init__(*args, **kwargs)
+        # Make it so the user can only select properties he is selling
         self.fields['property'].queryset = Properties.objects.filter(deleted=False, pk__in=tmp_list)
